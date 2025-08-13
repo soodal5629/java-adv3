53dc9682df5669227ccee1270d515945d965a7f5
chaelynjang <chaelynjang@lotte.net>
2025-08-13 21:30:00 +0900

    람다 생략 방법 확인

---
+++ src/lambda/labmda1/LambdaSimple3.java
@@ -0,0 +1,15 @@
+package lambda.labmda1;
+
+import lambda.MyFunction;
+
+public class LambdaSimple3 {
+    public static void main(String[] args) {
+        // 타입 생략 전
+        MyFunction myFunction1 = (int a, int b) -> a + b;
+        System.out.println("myFunction1 = " + myFunction1.apply(1, 2));
+
+        // MyFunction 타입을 통해 타입 추론 가능 -> 람다는 타입 생략 가능
+        MyFunction myFunction2 = (a, b) -> a + b;
+        System.out.println("myFunction2 = " + myFunction2.apply(3, 4));
+    }
+}
